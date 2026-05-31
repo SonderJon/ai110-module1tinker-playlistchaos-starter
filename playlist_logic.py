b143220b26d29eb235a205bf6db01ac032331724
Jonathan Pina-V <jon.pina25@gmail.com>
2026-05-31 15:21:32 -0700

    Bug 1

--- playlist_logic.py
+++ playlist_logic.py
@@ -167,8 +167,8 @@ def search_songs(
     filtered: List[Song] = []
 
     for song in songs:
-        value = str(song.get(field, "")).lower()
-        if value and value in q:
+        value = str(song.get(field, "")).lower() # gets all songs
+        if value and q in value: # original - if value and value in q:
             filtered.append(song)
 
     return filtered
